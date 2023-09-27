dbc7d9deb56da7909593385bf944a9784836ad6c
Levi <phamnguyentanphucgl@gmail.com>
2023-09-27 22:04:13 +0700

    add auto_start app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,5 @@
+import 'package:auto_start_flutter/auto_start_flutter.dart';
+
 import 'package:bot_toast/bot_toast.dart';
 
 import 'package:cardtap/src/localization/localization_utils.dart';
@@ -6,13 +8,43 @@ import 'package:cardtap/src/router/coordinator.dart';
 import 'package:cardtap/src/router/route_observer.dart';
 import 'package:cardtap/src/router/router_name.dart';
 import 'package:cardtap/src/theme/themes.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<ScaffoldMessengerState> _messengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  void initState() {
+    super.initState();
+    //call in init state;
+    initAutoStart();
+  }
+
+  //initializing the autoStart with the first build.
+  Future<void> initAutoStart() async {
+    try {
+      //check auto-start availability.
+      var test = await isAutoStartAvailable;
+
+      //if available then navigate to auto-start setting page.
+      if (test == true) await getAutoStartPermission();
+    } on PlatformException catch (e) {
+      debugPrint("initAutoStart: $e");
+    }
+    if (!mounted) return;
+  }
 
   @override
   Widget build(BuildContext context) {
